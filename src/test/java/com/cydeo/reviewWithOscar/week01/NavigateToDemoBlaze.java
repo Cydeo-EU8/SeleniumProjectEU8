553dc2ab9e757053130fd248c316f5c0e363f0c6
OscarSah <oscar@cybertekschool.com>
2022-04-30 19:36:55 +0300

    Review Test One

---
+++ src/test/java/com/cydeo/reviewWithOscar/week01/NavigateToDemoBlaze.java
@@ -0,0 +1,32 @@
+package com.cydeo.reviewWithOscar.week01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigateToDemoBlaze {
+
+    public static void main(String[] args) {
+
+        // https://www.demoblaze.com/index.html
+        // Verify Title "STORE"
+
+        // Analogy: Taxi Driver Example
+        // taxi car: browser-Chrome, taxiDriver: Selenium Webdriver- who can drive this particular car
+
+        WebDriverManager.chromedriver().setup(); // I am setting up my taxi driver
+
+        WebDriver driver = new ChromeDriver(); // the car itself --- This line opens an empty Chrome webPage
+        // there is polymorphisim: WebDriver is a interface , ChromeDriver is the concrete class
+
+        driver.get("https://www.demoblaze.com/index.html");
+
+      //  driver.navigate().to("https://www.demoblaze.com/index.html");
+
+        String expectedTitle = "STORE";
+        // we have getTitle() method in Selenium WebDriver library
+        String actualTitle = driver.getTitle()
+
+    }
+
+}
