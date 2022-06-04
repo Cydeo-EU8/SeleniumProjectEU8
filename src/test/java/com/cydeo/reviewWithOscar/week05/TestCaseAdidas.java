0e3736ef3139d2a1f2356c2bc8836995a3cd0598
OscarSah <oscar@cybertekschool.com>
2022-06-04 22:44:27 +0300

    Adidas Task Second Part

--- src/test/java/com/cydeo/reviewWithOscar/week05/TestCaseAdidas.java
+++ src/test/java/com/cydeo/reviewWithOscar/week05/TestCaseAdidas.java
@@ -4,6 +4,7 @@ import com.cydeo.tests.base.TestBase;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.ReviewUtils;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -19,7 +20,7 @@ public class TestCaseAdidas extends TestBase {
       expectedPrice += ReviewUtils.addProduct(driver,ConfigurationReader.getProperty("category2"),"Samsung galaxy s6");
         // go to Cart
        ReviewUtils.getLink(driver,"Cart");
-        ReviewUtils.staticWait(1); // PUT one second waiting time
+        ReviewUtils.staticWait(3); // PUT one second waiting time
       // First get Cart price then click on place order
      double cartPrice = Double.parseDouble(driver.findElement(By.id("totalp")).getText());
        System.out.println("cartPrice = " + cartPrice);
@@ -27,6 +28,32 @@ public class TestCaseAdidas extends TestBase {
        System.out.println("expectedPrice = " + expectedPrice);
 
        driver.findElement(By.xpath("//button[.='Place Order']")).click();
+       ReviewUtils.staticWait(2);
+
+       ReviewUtils.fillForm(driver);
+
+       String confirmationText = driver.findElement(By.xpath("//p[@class='lead text-muted ']")).getText();
+
+       String IDtext = driver.findElement(By.xpath("//p[@class='lead text-muted ']//br")).getText();
+       System.out.println("IDtext = " + IDtext);
+       System.out.println("confirmationText = " + confirmationText);
+       /*
+    Id: 8761753
+    Amount: 1150 USD
+    Card Number: 3436-094090-48661
+    Name: Mrs. Tracey Kuhlman
+    Date: 4/5/2022
+        */
+       
+       String ID = confirmationText.split("\n")[0];
+       double actualPrice = Double.parseDouble(confirmationText.split("\n")[1].split(" ")[1]);
+
+       System.out.println(ID);
+       System.out.println("actualPrice = " + actualPrice);
+
+       Assert.assertEquals(actualPrice,expectedPrice);
+       Assert.assertEquals(cartPrice,expectedPrice);
+       
 
    }
 
--- src/test/java/com/cydeo/utilities/ReviewUtils.java
+++ src/test/java/com/cydeo/utilities/ReviewUtils.java
@@ -1,9 +1,11 @@
 package com.cydeo.utilities;
 
+import com.github.javafaker.Faker;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.Date;
 import java.util.Set;
 
 public class ReviewUtils {
@@ -34,6 +36,19 @@ double price = Double.parseDouble(priceText.substring(1,4));
             Alert alert = driver.switchTo().alert();
             alert.accept();
             return price;
+    }
+
+    public static void fillForm(WebDriver driver){
+
+        Faker faker = new Faker();
+        driver.findElement(By.id("name")).sendKeys(faker.name().fullName());
+        driver.findElement(By.id("country")).sendKeys(faker.country().name());
+        driver.findElement(By.id("city")).sendKeys(faker.address().city());
+        driver.findElement(By.id("card")).sendKeys(faker.finance().creditCard());
+        driver.findElement(By.id("month")).sendKeys(""+faker.number().numberBetween(1,12));
+        driver.findElement(By.id("year")).sendKeys(""+faker.number().numberBetween(2022,2032));
+        driver.findElement(By.xpath("//button[.='Purchase']")).click();
+        staticWait(2);
     }
 
 
