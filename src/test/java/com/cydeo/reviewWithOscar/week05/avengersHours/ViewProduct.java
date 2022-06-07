b0c53c1b6348bc763a1901cf2698a80d6603f725
OscarSah <oscar@cybertekschool.com>
2022-06-07 21:57:31 +0300

    Click on random product and verify view page and change quantity to 4

--- src/test/java/com/cydeo/reviewWithOscar/week05/avengersHours/ViewProduct.java
+++ src/test/java/com/cydeo/reviewWithOscar/week05/avengersHours/ViewProduct.java
@@ -3,6 +3,7 @@ package com.cydeo.reviewWithOscar.week05.avengersHours;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -25,6 +26,16 @@ public class ViewProduct extends TestBaseForAvengers{
 
         Driver.getDriver().findElement(By.xpath("(//a[.='View Product'])["+randomProductNumber+"]")).click();
 
+        // Verify product detail is opened
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("product_details"));
+
+        WebElement quantityBox = Driver.getDriver().findElement(By.id("quantity"));
+        quantityBox.clear();
+        quantityBox.sendKeys("4");
+
+        int expectedValue = 4;
+        int actualValue = Integer.parseInt(quantityBox.getAttribute("value"));
+        Assert.assertEquals(actualValue,expectedValue);
 
     }
 
