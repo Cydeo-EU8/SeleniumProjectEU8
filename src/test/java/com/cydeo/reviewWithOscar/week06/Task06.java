645ef79be1cc5318354643ba56900d10c40cbd7c
OscarSah <oscar@cybertekschool.com>
2022-06-11 18:29:49 +0300

    Scrolling Ways

--- src/test/java/com/cydeo/reviewWithOscar/week06/Task06.java
+++ src/test/java/com/cydeo/reviewWithOscar/week06/Task06.java
@@ -2,8 +2,12 @@ package com.cydeo.reviewWithOscar.week06;
 
 import com.cydeo.reviewWithOscar.week06.pomForReview.ContactUsPage;
 import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.ReviewUtils;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,6 +25,27 @@ public class Task06 {
         // click contact us
         // page.contactUs.click();
         page.getElement("Contact us").click();
+
+        // for scrolling test cases
+        // create actions object
+        // first way
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.PAGE_DOWN,Keys.PAGE_DOWN).perform();
+        ReviewUtils.staticWait(2);
+        actions.sendKeys(Keys.PAGE_UP,Keys.PAGE_UP).perform();
+        ReviewUtils.staticWait(2);
+        // second way
+        actions.moveToElement(page.submit).perform();
+        // third way
+        JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+        jse.executeScript("window.scrollBy(0, 750)");
+        ReviewUtils.staticWait(1);
+        jse.executeScript("window.scrollBy(0, -750)");
+        ReviewUtils.staticWait(1);
+        // last way
+        jse.executeScript("arguments[0].scrollIntoView(true)",page.submit);
+        ReviewUtils.staticWait(2);
+
 
         Faker faker = new Faker();
 
