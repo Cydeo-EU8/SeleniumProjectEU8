8c6554519ec7060cddcd123841e8f5c912da2394
OscarSah <oscar@cybertekschool.com>
2022-06-14 20:48:46 +0300

    POM for Adidas

--- src/test/java/com/cydeo/reviewWithOscar/week06/avengersHours/AdidasBasePage.java
+++ src/test/java/com/cydeo/reviewWithOscar/week06/avengersHours/AdidasBasePage.java
@@ -3,6 +3,8 @@ package com.cydeo.reviewWithOscar.week06.avengersHours;
 import com.cydeo.utilities.Driver;
 import com.cydeo.utilities.ReviewUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class AdidasBasePage {
@@ -15,4 +17,15 @@ public abstract class AdidasBasePage {
         Driver.getDriver().findElement(By.partialLinkText(link)).click();
         ReviewUtils.staticWait(1);
     }
+   /*
+    public final String str = "Laptops"; // I made it constant
+    @FindBy(linkText = str) // it works only with constants
+    public WebElement getLinkElement;
+
+    */
+    public WebElement getLinkElement(String str){
+        return Driver.getDriver().findElement(By.linkText(str));
+    }
+
+
 }
